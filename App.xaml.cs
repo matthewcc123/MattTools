bc907a575879d1ff66d949e04b025a4dc8dd331c
matthewcc123 <matthew_cc@outlook.com>
2023-08-07 00:02:14 +0700

    First Commit

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MattTools;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+    public static Window Window { get; private set; }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window = base.CreateWindow(activationState);
+
+        Window.Width = 1280;
+        Window.Height = 832;
+        Window.Page = MainPage;
+
+        return Window;
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MattTools;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
